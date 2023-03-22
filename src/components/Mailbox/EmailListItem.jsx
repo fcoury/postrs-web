6352865a9fe1fe337d74a8ab007f8fb46634f956
Felipe Coury <felipe.coury@gmail.com>
2023-03-22 11:01:37 -0300

    feat: email list actions

--- src/components/Mailbox/EmailListItem.jsx
+++ src/components/Mailbox/EmailListItem.jsx
@@ -1,27 +1,33 @@
 import { stripHtml } from "string-strip-html";
 import friendlyDate from "../../utils/friendlyDate";
 import Avatar from "../Avatar";
+import styles from "./EmailListItem.module.css";
 
 export default function EmailListItem({ email, selected, onClick }) {
   return (
     <div
       key={email.internal_id}
       onClick={onClick}
-      className={`email-list-item${selected ? " selected" : ""}`}
+      className={`${styles.emailListItem}${selected ? " selected" : ""}`}
     >
       <Avatar name={email.from_name} email={email.from_addr} size={30} />
-      <div className="email-header">
-        <div className="received">
+      <div className={styles.emailHeader}>
+        <div className={styles.received}>
           <i className="far fa-paperclip"></i>
           {friendlyDate(email.date, true)}
         </div>
-        <div className="sender">{email.from_name}</div>
-        <div className="subject">{email.subject}</div>
-        <div className="action">
+        <div className={styles.sender}>{email.from_name}</div>
+        <div className={styles.subject}>{email.subject}</div>
+        <div className={styles.action}>
           <i className="far fa-star"></i>
         </div>
-        <div className="body">
+        <div className={styles.body}>
           {email.body ? stripHtml(email.body).result : "No preview available"}
+        </div>
+        <div className={styles.iconContainer}>
+          <i className={`far fa-archive ${styles.icon}`}></i>
+          <i className={`far fa-trash-alt ${styles.icon}`}></i>
+          <i className={`far fa-envelope-open ${styles.icon}`}></i>
         </div>
       </div>
     </div>
